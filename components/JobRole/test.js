2857b270fe56d9897e0fc9366af02b8af01a621e
TashDubrovska <n.dubrovska89@gmail.com>
2018-12-02 18:16:25 +0000

    Add JobRole component

---
+++ components/JobRole/test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import JobRole from '.';
+import data from '../../data.json';
+
+const baseClassName = 'job-experience__job-role';
+
+describe('JobRole', () => {
+    let jobRole;
+    let jobRoleData = data.work[0];
+
+    beforeAll(() => {
+        jobRole = shallow(<JobRole company={jobRoleData.company}
+                                   dates={jobRoleData.dates}
+                                   role={jobRoleData.role}
+                                   description={jobRoleData.description} />)
+    });
+
+    test('should render company name', () => {
+        expect(jobRole.find(`.${baseClassName}__company`).text()).toEqual(jobRoleData.company);
+    });
+
+    test('should render dates', () => {
+        expect(jobRole.find(`.${baseClassName}__duration`).text()).toEqual(jobRoleData.dates);
+    });
+
+    test('should render role name', () => {
+        expect(jobRole.find(`.${baseClassName}__role`).text()).toEqual(jobRoleData.role);
+    });
+
+    test('should render role description', () => {
+        expect(jobRole.find(`.${baseClassName}__description`).text()).toEqual(jobRoleData.description);
+    });
+});
