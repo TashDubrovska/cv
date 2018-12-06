9ee6968b4a71699c26023889783759e706c40b5b
TashDubrovska <n.dubrovska89@gmail.com>
2018-12-06 02:19:56 +0000

    Fixup how multiline skills are displayed

--- components/Skill/index.jsx
+++ components/Skill/index.jsx
@@ -6,7 +6,7 @@ const baseClassName = 'skill';
 const Skill = ({ name, additionalInfo, level }) => (
   <li className={baseClassName}>
     <h4 className={`${baseClassName}__name`}>{name}</h4>
-    {additionalInfo.length > 0 && <p className={`${baseClassName}__info`}>{` (${additionalInfo})`}</p>}
+    {additionalInfo.length > 0 && <p className={`${baseClassName}__info`}>{` - ${additionalInfo}`}</p>}
     <p className={`${baseClassName}__level`}>
       <span className={`${baseClassName}__level__highlight`} style={{ width: `${Number(level) / 5 * 100}%` }} />
       <span className="hide-a11y">{`${level} of 5`}</span>
