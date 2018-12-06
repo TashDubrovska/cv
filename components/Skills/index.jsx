4fe41c34ce8d856f788799e2e714aa016c4f02b4
TashDubrovska <n.dubrovska89@gmail.com>
2018-12-06 01:39:56 +0000

    Add styling for skill section

--- components/Skills/index.jsx
+++ components/Skills/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Skill from '../Skill';
 
+const baseClassName = 'skills';
+
 const Skills = ({ technical, personal }) => (
-  <section>
-    <h2>Skills</h2>
-    <div className="technical">
-      <h3>Technical</h3>
-      <ul>
+  <section className={baseClassName}>
+    <h2 className={`${baseClassName}__heading`}>Skills</h2>
+    <div className={`${baseClassName}__subsection technical`}>
+      <h3 className={`${baseClassName}__subsection__heading`}>Technical</h3>
+      <ul className={`${baseClassName}__subsection__list`}>
         {
           technical.map((skill, index) => (
             <Skill
@@ -21,9 +23,9 @@ const Skills = ({ technical, personal }) => (
       </ul>
     </div>
 
-    <div className="personal">
-      <h3>Personal</h3>
-      <ul>
+    <div className={`${baseClassName}__subsection personal`}>
+      <h3 className={`${baseClassName}__subsection__heading`}>Personal</h3>
+      <ul className={`${baseClassName}__subsection__list`}>
         {
           personal.map((skill, index) => (
             <Skill
