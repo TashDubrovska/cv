c997c51b4bca528594b6483349dd0fea89669776
TashDubrovska <n.dubrovska89@gmail.com>
2018-12-06 02:13:06 +0000

    Update skills in data

--- components/Skill/index.jsx
+++ components/Skill/index.jsx
@@ -8,7 +8,7 @@ const Skill = ({ name, additionalInfo, level }) => (
     <h4 className={`${baseClassName}__name`}>{name}</h4>
     {additionalInfo.length > 0 && <p className={`${baseClassName}__info`}>{` (${additionalInfo})`}</p>}
     <p className={`${baseClassName}__level`}>
-      <span className={`${baseClassName}__level__highlight`} style={{ width: `${level / 5 * 100}%` }} />
+      <span className={`${baseClassName}__level__highlight`} style={{ width: `${Number(level) / 5 * 100}%` }} />
       <span className="hide-a11y">{`${level} of 5`}</span>
     </p>
   </li>
