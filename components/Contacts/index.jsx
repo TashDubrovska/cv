7ccdd15be4257750170f322b5ac1cf395603b313
TashDubrovska <n.dubrovska89@gmail.com>
2018-12-15 01:19:12 +0000

    Add visible text for print style on contact links

--- components/Contacts/index.jsx
+++ components/Contacts/index.jsx
@@ -7,8 +7,9 @@ const Contacts = ({ contacts }) => (
   <ul className={baseClass}>
     {Object.keys(contacts).map((contact, index) => (
       <li key={index} className={`${baseClass}__contact`}>
-        <a className={`${baseClass}__contact__link`} href={contacts[contact]}>
+        <a className={`${baseClass}__contact__link`} href={contacts[contact].link}>
           <img className={`${baseClass}__contact__link__image`} src={`./components/Contacts/images/${contact}.svg`} alt="" />
+          <span className={`${baseClass}__contact__link__print`}>{contacts[contact].printText}</span>
           <span className="hide-a11y">{`Contact me via ${contact}`}</span>
         </a>
       </li>
