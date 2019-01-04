fd3aa28678a37e06b62b5f4e14118b635497f0a7
TashDubrovska <n.dubrovska89@gmail.com>
2019-01-04 00:56:27 +0000

    Move service worker activation to index

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ import App from './components/App/index';
 
 import data from './data.json';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('service-worker.js')
-    .then(() => console.log('Service worker is registered'));
-}
-
 render(
   <App data={data} />,
   document.getElementById('cv'),
